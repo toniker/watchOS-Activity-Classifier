6bc554edd6366d82a302d2794cda76e52fce888a
Antonis Keremidis <tkeremidis@gmail.com>
2024-04-05 13:54:14 +0300

    Initial Commit

---
+++ Activity Classifier/Activity_ClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Activity_ClassifierApp.swift
+//  Activity Classifier
+//
+//  Created by Antonis Keremidis on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Activity_ClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Activity Classifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Activity Classifier
+//
+//  Created by Antonis Keremidis on 5/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
